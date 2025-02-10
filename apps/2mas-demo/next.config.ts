73b46235500509edec8a54b6db6687474d6adfe7
Tomas Jansson <tomas.jansson@gmail.com>
2025-02-10 10:15:37 +0100

    More stuff

--- apps/2mas-demo/next.config.ts
+++ apps/2mas-demo/next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: "/2mas-demo-static",
+  async rewrites() {
+    return [
+      {
+        source: "/login",
+        destination: `${process.env.AUTH_DOMAIN}/login`,
+      },
+      {
+        source: "/logout",
+        destination: `${process.env.AUTH_DOMAIN}/logout`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
