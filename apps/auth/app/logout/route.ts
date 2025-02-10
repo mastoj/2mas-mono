c7d2194cd4a1c8e47bb3d7db5dd6c52fb337cf72
Tomas Jansson <tomas.jansson@gmail.com>
2025-02-10 23:12:15 +0100

    Logout route

--- apps/auth/app/logout/route.ts
+++ apps/auth/app/logout/route.ts
@@ -1,5 +1,22 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json({ message: "Hello, World!" });
+// Docs: https://learn.microsoft.com/en-us/entra/identity-platform/v2-oauth2-auth-code-flow
+
+// Creates the login url for entra id app
+const getLogoutUrl = (returnUrl: string) => {
+  const tenantId = process.env.ENTRA_TENANT_ID;
+  const state = { returnUrl };
+  const base64State = Buffer.from(JSON.stringify(state)).toString("base64");
+  const redirectUri = `${process.env.APP_DOMAIN}/logout/callback`;
+
+  const url = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/logout?post_logout_redirect_uri${redirectUri}&state=${base64State}`;
+
+  return url;
+};
+
+export async function GET(req: NextRequest) {
+  const appDomain = process.env.APP_DOMAIN;
+  const returnUrl =
+    req.nextUrl.searchParams.get("returnUrl") || `${appDomain}/`;
+  return NextResponse.redirect(getLogoutUrl(returnUrl));
 }
