d25c81de172e9f3b3c1cd40d324a317ba26d2cd8
Tomas Jansson <tomas.jansson@gmail.com>
2025-02-10 11:39:04 +0100

    Ignore tmp folder

--- apps/auth/app/login/route.ts
+++ apps/auth/app/login/route.ts
@@ -1,5 +1,18 @@
 import { NextResponse } from "next/server";
 
+// Creates the login url for entra id app
+const getLoginUrl = (redirect: string) => {
+  const clientId = process.env.CLIENT_ID;
+  const domain = process.env.AUTH_DOMAIN;
+  const responseType = "code";
+  const scope = "openid profile email";
+  const state = "1234567890";
+  const nonce = "0987654321";
+  const url = `${domain}/authorize?client_id=${clientId}&response_type=${responseType}&scope=${scope}&state=${state}&nonce=${nonce}&redirect_uri=${redirect}`;
+  return url;
+};
+
 export async function GET() {
-  return NextResponse.json({ message: "Hello, World!" });
+  const redirect = `${process.env.BASE_URL}/auth`;
+  return NextResponse.redirect(`${process.env.AUTH_DOMAIN}/logout`);
 }
